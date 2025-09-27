349624f9e2ba7a676e3b22100523bad4144ae885
Kevin Gege <kcampodonico@unprg.edu.pe>
2025-09-27 17:37:26 -0500

    first commit

---
+++ src/index.ts
@@ -0,0 +1,56 @@
+/* import express, { type Express } from "express";
+import type { Request, Response } from "express";
+
+const app: Express = express();
+const port = process.env.PORT ?? 3000;
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("Hello from TypeScript Express server!");
+});
+
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
+});
+ */
+
+import 'dotenv/config';
+import { drizzle } from 'drizzle-orm/node-postgres';
+import { eq } from 'drizzle-orm';
+import { usersTable } from './db/schema.js'; 
+  
+const db = drizzle(process.env.DATABASE_URL!);
+
+async function main() {
+  const user: typeof usersTable.$inferInsert = {
+    name: 'John',
+    age: 30,
+    email: 'john@example.com',
+  };
+
+  await db.insert(usersTable).values(user);
+  console.log('New user created!')
+
+  const users = await db.select().from(usersTable);
+  console.log('Getting all users from the database: ', users)
+  /*
+  const users: {
+    id: number;
+    name: string;
+    age: number;
+    email: string;
+  }[]
+  */
+
+  await db
+    .update(usersTable)
+    .set({
+      age: 31,
+    })
+    .where(eq(usersTable.email, user.email));
+  console.log('User info updated!')
+
+  await db.delete(usersTable).where(eq(usersTable.email, user.email));
+  console.log('User deleted!')
+}
+
+main();
